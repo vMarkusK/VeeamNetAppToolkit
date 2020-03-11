6e44e43d9f765ea146d30aa75f52d66b41def603
Markus Kraus <markus.kraus@gmail.com>
2020-03-11 23:14:15 +0100

    Add Example

--- functions/New-VeeamNetappVolume.psm1
+++ functions/New-VeeamNetappVolume.psm1
@@ -13,6 +13,10 @@ function New-VeeamNetappVolume {
         https://mycloudrevolution.com/
 
     .EXAMPLE
+    New-VeeamNetappVolume -VeeamCacheRepo 'Default Backup Repository' -VolType NFS -IP 10.0.2.16 `
+    -ExportPolicyName veeam -VolName vol_nfs_001 -VolSize 1 -NetAppAggregate aggr1_data01 `
+    -NetAppVserver svm_veeam_nfs -NetAppInterface svm_veeam_nfs_nfs_lif1 -NetAppSnapshotPolicy default
+
 
     .PARAMETER Type
     Type of the new Volume
